999a383ec317f5060eca57abaae9a034dba81d5f
Taher Mahmud Monmoy <monmoyzx@gmail.com>
2025-10-05 12:44:38 +0600

    Time: 21 ms (5.14%), Space: 45.1 MB (6.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        String revStr = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            revStr = str.charAt(i) + revStr;
+        }
+
+        if(str.equals(revStr)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
